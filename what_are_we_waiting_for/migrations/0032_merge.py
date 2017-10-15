76e63bc3738f71e91a2ca45c59eb87a2a276fb8f
chris <christian.humphreys@gmail.com>
2017-10-15 10:38:46 +0100

    fixing migrtions and no need for postgresql

---
+++ what_are_we_waiting_for/migrations/0032_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('what_are_we_waiting_for', '0001_initial'),
+        ('what_are_we_waiting_for', '0031_auto_20170719_1018'),
+    ]
+
+    operations = [
+    ]
